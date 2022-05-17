83b8a6a85643cb7703b4a7b487c187aec0f3d2af
juanzitelli <juanzitelli7@gmail.com>
2022-05-17 15:47:39 -0300

    add kanban entities

--- app/_types/entities.ts
+++ app/_types/entities.ts
@@ -1,5 +1,7 @@
 import type { definitions } from "./supabase";
 
 export type Organization = definitions["organizations"];
-
 export type Team = definitions["teams"];
+export type Task = definitions["tasks"];
+export type Board = definitions["boards"];
+export type BoardState = definitions["boardStates"];
