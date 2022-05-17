d7459411bc91071e9ef8058b1828089f5c916257
juanzitelli <juanzitelli7@gmail.com>
2022-05-17 15:52:48 -0300

    update single board page to use client context

--- app/routes/$product/$orgSlug/$teamSlug/kanban/$boardId.tsx
+++ app/routes/$product/$orgSlug/$teamSlug/kanban/$boardId.tsx
@@ -1,14 +1,45 @@
+import type { LoaderFunction, RouteComponent } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import type { LoaderFunction } from "@remix-run/node";
+import { useContext, useEffect } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { Board, StateTray } from "~/components/modules/decode";
+import { SupabaseClientContext } from "~/db";
 import { Board, StateTray, Task } from "~/components/modules/decode";
+import type { Task } from "~/_types";
+
+interface LoaderData {
+  data: Array<Task> | null;
+}
 
 export const loader: LoaderFunction = async () => {
-  return json({});
+  const { data } = await getTasksByStateId({ stateId: 1 });
+
+  return json<LoaderData>({
+    data,
+  });
 };
 
-const BoardRoute = () => {
+const BoardRoute: RouteComponent = () => {
+  const supabase = useContext(SupabaseClientContext);
+
+  useEffect(() => {
+    if (!supabase) return;
+
+    const subscription = supabase
+      .from("tasks")
+      .on("INSERT", (payload) => {
+        console.log("Change received!", payload);
+      })
+      .subscribe();
+
+    return () => {
+      subscription.unsubscribe();
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <DndProvider backend={HTML5Backend}>
       <Board>
