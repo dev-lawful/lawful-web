fb52eb10f75d55366141f60e0ce50442ae3538cc
Gianfranco Elli <gianelli99@hotmail.com>
2022-05-25 18:57:30 -0300

    BOOOM, react-markdown work, delete old markdown parsers is left

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,23 @@
+import { Heading } from "@chakra-ui/react";
+import ReactMarkdown from "react-markdown";
+
+const markdown = `# Here is some JavaScript code:
+
+~~~js
+console.log('It works!')
+~~~
+`;
+
 export default function IndexRoute() {
   return (
     <div>
       <h1>Landing Lawful</h1>
+      <ReactMarkdown
+        children={markdown}
+        components={{
+          h1: (props) => <Heading as="h1" size="xl" {...props} />,
+        }}
+      />
     </div>
   );
 }
