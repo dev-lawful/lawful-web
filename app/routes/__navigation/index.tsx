151262ac9b29da70d85adfd0d67c4b38d006bf98
juanzitelli <juanzitelli7@gmail.com>
2022-07-17 00:19:06 -0300

    add lawful landing page

--- app/routes/__navigation/index.tsx
+++ app/routes/__navigation/index.tsx
@@ -1,3 +1,72 @@
+import {
+  Box,
+  Button,
+  Container,
+  createIcon,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+import { Link as RemixLink } from "@remix-run/react";
+
 export default function IndexRoute() {
-  return <h1>Lawful landing</h1>;
+  return (
+    <>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            One app. <br />
+            <Text as={"span"} color={"lawful.400"}>
+              Many collaboration tools.
+            </Text>
+          </Heading>
+          <Text color={"gray.500"}>
+            Lawful re-invents collaboration tooling allowing your organization
+            to move <strong>fast</strong>, spend <strong>less</strong> and get
+            more things <strong>done</strong>.
+          </Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
+            <Button
+              as={RemixLink}
+              to="/signup"
+              colorScheme={"lawful"}
+              bg={"lawful.400"}
+              rounded={"full"}
+              px={6}
+              _hover={{
+                bg: "lawful.500",
+              }}
+            >
+              Sign up
+            </Button>
+            <Button
+              as={RemixLink}
+              to="/signup"
+              variant={"link"}
+              colorScheme={"blue"}
+              size={"sm"}
+            >
+              Already an user? Sign in
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 }
