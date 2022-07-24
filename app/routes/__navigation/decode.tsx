f6562442c0bdc448c12ebe242bfc6db974801fb1
juanzitelli <juanzitelli7@gmail.com>
2022-07-24 10:01:57 -0300

    add products landing pages

--- app/routes/__navigation/decode.tsx
+++ app/routes/__navigation/decode.tsx
@@ -1,5 +1,57 @@
+import {
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
 const DecodeAboutRoute = () => {
-  return <div>DecodeAboutRoute</div>;
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+            <Text
+              as={"span"}
+              position={"relative"}
+              _after={{
+                content: "''",
+                width: "full",
+                height: useBreakpointValue({ base: "20%", md: "30%" }),
+                position: "absolute",
+                bottom: 1,
+                left: 0,
+                bg: "decode.400",
+                zIndex: -1,
+              }}
+            >
+              Decode
+            </Text>
+            <br />{" "}
+            <Text color={"decode.400"} as={"span"}>
+              Agile software development made easy.
+            </Text>{" "}
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+            Communication in modern software development is key.That's why{" "}
+            <Text as="strong">Decode</Text> brings top-notch task management and
+            decision-taking features to the table.
+          </Text>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
+  );
 };
 
 export default DecodeAboutRoute;
