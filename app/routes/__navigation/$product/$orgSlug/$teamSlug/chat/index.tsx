8a522f0dae39f7378852644cec7640255a380b44
Gianfranco Elli <gianelli99@hotmail.com>
2022-07-25 20:14:44 -0300

    Add chat index route

--- app/routes/__navigation/$product/$orgSlug/$teamSlug/chat/index.tsx
+++ app/routes/__navigation/$product/$orgSlug/$teamSlug/chat/index.tsx
@@ -1,7 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const ChatIndexRoute = () => {
-  return <Text>Hey! Choose a chat and start talking...</Text>;
+  return (
+    <Box textAlign="center" flex={1}>
+      <InfoIcon boxSize={"50px"} color={"blue.500"} />
+      <Heading as="h2" size="xl" mt={6} mb={2}>
+        Welcome to the Chat main page!
+      </Heading>
+      <Text color={"gray.500"}>
+        Click on one of the chats listed in the sidebar on the left side of the
+        screen and start messaging!
+      </Text>
+    </Box>
+  );
 };
 
 export default ChatIndexRoute;
