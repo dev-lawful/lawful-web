ea87e5b458557641ca8a5af9e33ed73a7ac50f9a
Gianfranco Elli <gianelli99@hotmail.com>
2022-07-25 17:46:48 -0300

    add reset password functionality

--- app/routes/__auth/resetPassword.tsx
+++ app/routes/__auth/resetPassword.tsx
@@ -1,4 +1,6 @@
 import {
+  Alert,
+  AlertIcon,
   Button,
   Flex,
   FormControl,
@@ -8,23 +10,96 @@ import {
   Stack,
   useColorModeValue,
 } from "@chakra-ui/react";
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useLocation,
+  useTransition,
+} from "@remix-run/react";
+import { CustomCatchBoundary, CustomErrorBoundary } from "~/components/ui";
+import { supabase } from "~/db";
 
-interface ResetPasswordForm {
-  email: string;
-  password: string;
+interface ActionData {
+  formResult?: FormResult;
+}
+interface FormResult {
+  status: "error" | "success";
+  message: string;
 }
 
-export const loader: LoaderFunction = ({ request }) => {
-  const url = new URL(request.url);
-  return json({ url });
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const accessToken = formData.get("accessToken");
+
+  if (
+    !email ||
+    typeof email !== "string" ||
+    !password ||
+    typeof password !== "string" ||
+    !accessToken ||
+    typeof accessToken !== "string"
+  ) {
+    return json<ActionData>(
+      {
+        formResult: {
+          status: "error",
+          message: "Form not submitted correctly.",
+        },
+      },
+      { status: 400 }
+    );
+  }
+
+  const { error } = await supabase.auth.api.updateUser(accessToken, {
+    password,
+  });
+
+  if (error) {
+    return json<ActionData>(
+      {
+        formResult: {
+          status: "error",
+          message: error?.message,
+        },
+      },
+      { status: 400 }
+    );
+  }
+
+  return json<ActionData>({
+    formResult: {
+      status: "success",
+      message: "Congrats! Your password has been changed correctly.",
+    },
+  });
 };
 
 const ResetPasswordPage = () => {
-  const url = useLoaderData();
-  console.log(url);
+  const transition = useTransition();
+  const actionData = useActionData<ActionData>();
+
+  const { hash } = useLocation();
+  const [, fromAccessTokenToEndHash] = hash.split("access_token=", 2);
+  const [accessToken] = fromAccessTokenToEndHash?.split("&", 1) || [
+    fromAccessTokenToEndHash,
+  ];
+  const accessTokenAlert: FormResult | undefined = accessToken
+    ? undefined
+    : {
+        status: "error",
+        message: "Access token not found",
+      };
+  const formAlert = actionData?.formResult || accessTokenAlert;
+
+  const buttonDisabled =
+    transition.state === "submitting" ||
+    actionData?.formResult?.status === "success" ||
+    !accessToken;
+
   return (
     <Flex
       minH={"100vh"}
@@ -45,33 +120,48 @@ const ResetPasswordPage = () => {
         <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
           Enter new password
         </Heading>
-        <FormControl id="email" isRequired>
-          <FormLabel>Email address</FormLabel>
-          <Input
-            name="email"
-            placeholder="your-email@example.com"
-            _placeholder={{ color: "gray.500" }}
-            type="email"
-          />
-        </FormControl>
-        <FormControl id="password" isRequired>
-          <FormLabel>Password</FormLabel>
-          <Input type="password" name="password" />
-        </FormControl>
-        <Stack spacing={6}>
-          <Button
-            bg={"blue.400"}
-            color={"white"}
-            _hover={{
-              bg: "blue.500",
-            }}
-          >
-            Submit
-          </Button>
-        </Stack>
+        {formAlert ? (
+          <Alert status={formAlert.status} borderRadius={5}>
+            <AlertIcon />
+            {formAlert.message}
+          </Alert>
+        ) : null}
+        <Form method="post">
+          <Input type="hidden" name="accessToken" value={accessToken || ""} />
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                name="email"
+                placeholder="your-email@example.com"
+                _placeholder={{ color: "gray.500" }}
+                type="email"
+              />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" name="password" />
+            </FormControl>
+            <Button
+              type="submit"
+              disabled={buttonDisabled}
+              bg={"blue.400"}
+              color={"white"}
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Form>
       </Stack>
     </Flex>
   );
 };
 
 export default ResetPasswordPage;
+
+export const ErrorBoundary = CustomErrorBoundary;
+
+export const CatchBoundary = CustomCatchBoundary;
