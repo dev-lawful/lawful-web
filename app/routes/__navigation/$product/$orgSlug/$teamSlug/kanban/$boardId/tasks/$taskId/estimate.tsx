ff50265b5390ca9d82066717462f74fcf58fcafd
juanzitelli <juanzitelli7@gmail.com>
2022-12-08 00:46:33 -0300

    add calculation functions

--- app/routes/__navigation/$product/$orgSlug/$teamSlug/kanban/$boardId/tasks/$taskId/estimate.tsx
+++ app/routes/__navigation/$product/$orgSlug/$teamSlug/kanban/$boardId/tasks/$taskId/estimate.tsx
@@ -272,3 +272,38 @@ const EstimateTaskRoute = () => {
 };
 
 export default EstimateTaskRoute;
+
+const calculateMedian = (arr: Array<number>) => {
+  const { length } = arr;
+
+  arr.sort((a, b) => a - b);
+
+  if (length % 2 === 0) {
+    return (arr[length / 2 - 1] + arr[length / 2]) / 2;
+  }
+
+  return arr[(length - 1) / 2];
+};
+
+const calculateMode = (arr: Array<number>) => {
+  const mode: Record<string, number> = {};
+  let max = 0,
+    count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+
+    if (mode[item]) {
+      mode[item]++;
+    } else {
+      mode[item] = 1;
+    }
+
+    if (count < mode[item]) {
+      max = item;
+      count = mode[item];
+    }
+  }
+
+  return max;
+};
